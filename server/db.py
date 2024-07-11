c25b046589251b4b9d25c5e52cc4fef6dcfbcab1
rhit-bhuwalk <bhuwalk@rose-hulman.edu>
2024-07-10 19:16:21 -0500

    updated fly and env

--- server/db.py
+++ server/db.py
@@ -3,9 +3,8 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 from database.BASE import BaseDatabaseOperation
 import os
-
+import certifi
 load_dotenv(verbose=True, override=True)
-
 
 mongodb_client = None
 db = None
@@ -15,7 +14,7 @@ DB_ENV = os.environ.get("DB_ENV")
 
 async def connect_to_mongo():
     global mongodb_client, db
-    mongodb_client = AsyncIOMotorClient(MONGO_URL)
+    mongodb_client = AsyncIOMotorClient(MONGO_URL, tlsCAFile=certifi.where())
 
     if DB_ENV and DB_ENV == "prod":
         db = mongodb_client.UserInfo
