e5cd57dee13fd0411c62f3d9d085ea3bf7c6c2af
Selvam43 <muthuselvam.m99@gmail.com>
2024-08-14 13:07:44 +0530

    Mock image function

--- server/ai_models/utils.py
+++ server/ai_models/utils.py
@@ -28,49 +28,53 @@ key = os.environ.get("OPENAI_KEY")
 client = AsyncOpenAI(api_key=key)
 
 async def generate_prompts(prompts: List[str], numberOfPrompts: int):
-		try:
-			prompts_per_theme = numberOfPrompts // len(prompts)
-			extra_prompts = numberOfPrompts % len(prompts)
+	try:
+		prompts_per_theme = numberOfPrompts // len(prompts)
+		extra_prompts = numberOfPrompts % len(prompts)
 
-			generated_prompts = []
-			for i, prompt in enumerate(prompts):
-				num_prompts_for_this_theme = prompts_per_theme + (1 if i < extra_prompts else 0)
+		generated_prompts = []
+		for i, prompt in enumerate(prompts):
+			num_prompts_for_this_theme = prompts_per_theme + (1 if i < extra_prompts else 0)
 
-				messages = [
-					{
-						"role": "system",
-						"content": f"""
-						You are a prompt engineering assistant with a focus on optimizing prompts for generating 
-						high-quality images. You will be given a user prompt and the number of prompts to generate, 
-						and you must return JSON with {num_prompts_for_this_theme} enhanced prompts. 
-						The enhanced prompts should include every word of the original themes and have 15-25 words, 
-						and split the themes with the number of prompts. Remember to only return valid JSON, 
-						no more or less than {num_prompts_for_this_theme} prompts. Your suggestions should increase 
-						the original themes' specificity, uniqueness, different from other prompts, and detail 
-						to generate vivid and engaging images. The designs should be aesthetic and visually appealing.
+			messages = [
+				{
+					"role": "system",
+					"content": f"""
+					You are a prompt engineering assistant with a focus on optimizing prompts for generating 
+					high-quality images. You will be given a user prompt and the number of prompts to generate, 
+					and you must return JSON with {num_prompts_for_this_theme} enhanced prompts. 
+					The enhanced prompts should include every word of the original themes and have 15-25 words, 
+					and split the themes with the number of prompts. Remember to only return valid JSON, 
+					no more or less than {num_prompts_for_this_theme} prompts. Your suggestions should increase 
+					the original themes' specificity, uniqueness, different from other prompts, and detail 
+					to generate vivid and engaging images. The designs should be aesthetic and visually appealing.
+     
+     				The structure should be as follows:
+					{{
+						"Prompts": ["Prompt1", "Prompt2", ...., "Prompt{num_prompts_for_this_theme}"]
+					}}
+					"""
+				},
+				{
+					"role": "user",
+					"content": f"Theme: ['{prompt}'], NumberOfPrompts: {num_prompts_for_this_theme}"
+				}
+			]
 
-						The structure should be as follows:
-						{{
-							"Prompts": ["Prompt1", "Prompt2", ...., "Prompt{num_prompts_for_this_theme}"]
-						}}
-						"""
-					},
-					{
-						"role": "user",
-						"content": f"Theme: ['{prompt}'], NumberOfPrompts: {num_prompts_for_this_theme}"
-					}
-				]
+			completion = await client.chat.completions.create(
+				model="gpt-3.5-turbo",
+				messages=messages,
+				temperature=0.7
+			)
 
-				completion = await client.chat.completions.create(
-					model="gpt-3.5-turbo",
-					messages=messages,
-					temperature=0.7
-				)
+			response = completion.choices[0].message.content
+			response_json = json.loads(response)
+			generated_prompts.extend(response_json['Prompts'])
 
-			return generated_prompts
-		except openai.OpenAIError as e:
-			handle_openai_error(e)
-			return {"Prompts": []}
+		return generated_prompts
+	except openai.OpenAIError as e:
+		handle_openai_error(e)
+		return {"Prompts": []}
 async def generate_three_prompts(prompts: str, numberOfPrompts: int):
 	try:
 		prompts_per_theme = numberOfPrompts // len(prompts)
@@ -123,16 +127,14 @@ async def generate_three_prompts(prompts: str, numberOfPrompts: int):
 async def generate_images(
 	prompts: List[str]
 ):
+	DB_ENV = os.environ.get("DB_ENV")
+	if DB_ENV and DB_ENV == "prod":
+		ai_model_primary = TitanImageGenerator()
+	else:
+		ai_model_primary = MockTitanImageGenerator()
 	try:
-		DB_ENV = os.environ.get("DB_ENV")
-		if DB_ENV and DB_ENV == "prod":
-			ai_model_primary = TitanImageGenerator()
-			tasks = [ai_model_primary.generate_single_image(idx, prompt) for idx, prompt in enumerate(prompts)]
-			return await asyncio.gather(*tasks, return_exceptions=True)
-		else:
-			ai_model_primary = MockTitanImageGenerator()
-			tasks = [ai_model_primary.generate_single_image(idx, prompt) for idx, prompt in enumerate(prompts)]
-			return await asyncio.gather(*tasks, return_exceptions=True)
+		tasks = [ai_model_primary.generate_single_image(idx, prompt) for idx, prompt in enumerate(prompts)]
+		return await asyncio.gather(*tasks, return_exceptions=True)
 	except openai.OpenAIError as e:
 		handle_openai_error(e)
 	except HTTPException as http_exc:
